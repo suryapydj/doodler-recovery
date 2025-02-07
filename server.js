6856a666ecb84d56c8f6c276c65d843c39d2f440
Tim <timbits.r@gmail.com>
2025-02-07 10:20:33 -0500

    update server.js to use new folder name "client"

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "clerk-vue", "dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "clerk-vue", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 const generateRoomCode = () => {

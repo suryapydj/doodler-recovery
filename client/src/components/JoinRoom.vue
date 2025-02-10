89534e24c88d9b89daf1425b3ae547d1f52039b2
T8126 <timbits.r@gmail.com>
2025-02-09 22:37:20 -0500

    new components for join and create room pages, with limited functionality

---
+++ client/src/components/JoinRoom.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input v-model="roomCode" placeholder="Enter Room Code" />
+        <button @click="" :disabled="!roomCode">Join Room</button>
+        <button @click="backHome">Back Home</button>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from "vue-router";
+
+const roomCode = ref('');
+const router = useRouter();
+
+const backHome = () => {
+  router.push('/');
+};
+</script>
